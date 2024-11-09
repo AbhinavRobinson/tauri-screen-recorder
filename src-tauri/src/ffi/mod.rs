c7e7e7d72d0d247a64e6f8eab8d75eb9d2c72100
Aniket Chowdhury <8.aniket.chowdhury@gmail.com>
2024-11-09 23:27:28 +0530

    fix: allow record and store in memory

--- src-tauri/src/ffi/mod.rs
+++ src-tauri/src/ffi/mod.rs
@@ -1,3 +1,9 @@
+use std::{
+    any::Any,
+    sync::{mpsc, Arc, Mutex},
+    time::Instant,
+};
+
 use core_foundation::{
     base::{CFType, TCFType},
     boolean::CFBoolean,
@@ -8,7 +14,7 @@ use core_foundation::{
 };
 use core_graphics2::{
     display::{CGDisplayBounds, CGDisplayCreateImage, CGMainDisplayID},
-    display_stream::CGDisplayStream,
+    display_stream::{CGDisplayStream, CGDisplayStreamFrameStatus},
 };
 use core_video::{
     buffer::TCVBuffer,
@@ -37,7 +43,7 @@ pub fn ffi_loop() {
     println!("Width: {:?}", output_width);
     println!("Height: {:?}", output_height);
     let pixel_format = 1111970369;
-    let fps = 1f64;
+    let fps = 1.0 / 60.0;
     let properties: CFDictionary<CFString, CFType> = CFDictionary::from_CFType_pairs(&[
         (
             CFString::from_static_string("kCGDisplayStreamShowCursor"),
@@ -51,38 +57,45 @@ pub fn ffi_loop() {
             CFString::from_static_string("kCGDisplayStreamPreserveAspectRatio"),
             CFBoolean::true_value().as_CFType(),
         ),
+        (
+            CFString::from_static_string("kCGDisplayStreamQueueDepth"),
+            CFNumber::from(60).as_CFType(),
+        ),
     ]);
 
     let queue = Queue::new("label", QueueAttribute::Serial);
 
+    let (pixel_send, pixel_recv) = mpsc::channel();
+
+    let pixel_send = Arc::new(pixel_send);
+
+    let ins = Instant::now();
+
     let resp = CGDisplayStream::new_with_dispatch_queue(
         display,
-        output_width,
-        output_height,
+        output_width * 2,
+        output_height * 2,
         pixel_format,
         &properties,
         &queue,
-        |status, timestamp, iosurface, update| {
-            println!("Status: {:?}", status);
-            println!("Timestamp: {:?}", timestamp);
+        move |status, timestamp, iosurface, update| {
             // let mut data = [];
-            let surface = iosurface.unwrap();
+            let surface = iosurface.expect("Surface is None");
+            if status != CGDisplayStreamFrameStatus::FrameComplete {
+                eprintln!("Error: {:?}", status);
+                return;
+            }
             let update = update.unwrap();
             let h;
             let w;
             unsafe {
                 h = IOSurfaceGetHeight(surface.as_concrete_TypeRef());
-                println!("Height: {:?}", h);
                 w = IOSurfaceGetWidth(surface.as_concrete_TypeRef());
-                println!("Width: {:?}", w);
             }
-
-            println!("Update: {:?}\n", update.drop_count());
 
             let pb = CVPixelBuffer::from_io_surface(&surface, None).unwrap();
 
             let bytes_per_row = pb.get_bytes_per_row();
-            println!("Bytes Per Row: {:?}", bytes_per_row);
 
             pb.lock_base_address(kCVPixelBufferLock_ReadOnly);
 
@@ -90,7 +103,6 @@ pub fn ffi_loop() {
 
             unsafe {
                 base_address = pb.get_base_address();
-                println!("Base Address: {:?}", base_address);
             }
             let total_bytes = h * bytes_per_row;
 
@@ -105,25 +117,16 @@ pub fn ffi_loop() {
                 );
             }
 
+            if ins.elapsed().as_secs() > 5 {
+                return;
+            }
             pb.unlock_base_address(kCVPixelBufferLock_ReadOnly);
 
-            let mut img = image::RgbaImage::new(w as u32, h as u32);
-
-            for y in 0..h {
-                for x in 0..w {
-                    let index = (y * bytes_per_row + x * 4) as usize;
-                    let r = data[index];
-                    let g = data[index + 1];
-                    let b = data[index + 2];
-                    let a = data[index + 3];
-                    img.put_pixel(x as u32, y as u32, image::Rgba([b, g, r, a]));
-                }
-            }
-
-            img.save("test.png").unwrap();
-
-            todo!("Handle update");
-            // _o.unwrap();
+            pixel_send
+                .send((data, bytes_per_row, h, w, timestamp))
+                .unwrap();
+            println!("sending")
+            // pb.unlock_base_address(kCVPixelBufferLock_ReadOnly);
         },
     );
 
@@ -138,7 +141,43 @@ pub fn ffi_loop() {
         // std::thread::sleep(std::time::Duration::from_secs(5));
         // stream.stop();
 
-        CFRunLoop::run_current();
+        let t1 = std::thread::spawn(|| {
+            CFRunLoop::run_current();
+        });
+
+        // let duration = std::time::Duration::from_secs(5);
+
+        let mut count = 0;
+        let img_array = Arc::new(Mutex::new(Vec::new()));
+
+        loop {
+            let received = pixel_recv.try_recv();
+
+            if received.is_ok() {
+                count += 1;
+
+                let (data, bytes_per_row, h, w, _timestamp) = received.unwrap();
+
+                let img_array = img_array.clone();
+
+                let t = std::thread::spawn(move || {
+                    println!("Received in");
+
+                    let x = &mut img_array.lock().as_mut().unwrap().push(data);
+
+                    println!("Received: {:?}", img_array.lock().as_ref().unwrap().len());
+                });
+
+                t.join().unwrap();
+                // t.join().unwrap();
+            }
+        }
+
+        // t1.join().unwrap();
+
+        // queue.suspend();
+
+        // stream.stop();
 
         // if b.try_recv().is_ok() {
         //     println!("Received");
